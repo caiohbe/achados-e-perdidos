0d423d12508c280bd0c5f0da5a8faf65a258bb8d
unknown <caiohbe@gmail.com>
2025-06-19 14:54:59 -0300

    fix: added string condition to @transform validator from CreateUserDTO cpf field

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -15,11 +15,15 @@ export class CreateUserDto {
   @IsString()
   nome: string;
 
-  @Transform(({ value }) => value.replace(/\D/g, ''))
+  @Transform(({ value }) =>
+    typeof value == 'string' ? value.replace(/\D/g, '') : value,
+  )
   @Length(11, 11, { message: 'O CPF deve ter 11 dígitos' })
   @IsNumberString({}, { message: 'O CPF deve conter apenas números.' })
+  @IsString({ message: 'O CPF deve ser uma string.' })
   cpf: string;
 
   @IsEmail({}, { message: 'Email inválido.' })
+  @IsString({ message: 'Email deve ser uma string.' })
   email: string;
 }
