6026f4fedd164e71729f5629e43e70b5007620c3
unknown <caiohbe@gmail.com>
2025-06-21 00:48:43 -0300

    feature: handle prisma error try catch

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -3,34 +3,55 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from '@prisma/client';
 import { UpdateUserDto } from './dto/update-user.dto';
+import handlePrismaError from 'src/common/errors/prisma-error.handler';
 
 @Injectable()
 export class UsersRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async create({ nome, email, cpf }: CreateUserDto) {
-    return this.prisma.user.create({
-      data: { nome, email, cpf },
-    });
+    try {
+      await this.prisma.user.create({
+        data: { nome, email, cpf },
+      });
+    } catch (error) {
+      handlePrismaError(error);
+    }
   }
 
-  async findAll(): Promise<User[]> {
-    return this.prisma.user.findMany();
+  findAll(): Promise<User[]> {
+    try {
+      return this.prisma.user.findMany();
+    } catch (error) {
+      handlePrismaError(error);
+    }
   }
 
   async findOne(cpf: string): Promise<User> {
-    return this.prisma.user.findUnique({
-      where: {
-        cpf,
-      },
-    });
+    try {
+      return this.prisma.user.findUnique({
+        where: {
+          cpf,
+        },
+      });
+    } catch (error) {
+      handlePrismaError(error);
+    }
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
-    return this.prisma.user.update({ data: updateUserDto, where: { id } });
+    try {
+      return this.prisma.user.update({ data: updateUserDto, where: { id } });
+    } catch (error) {
+      handlePrismaError(error);
+    }
   }
 
   async remove(id: number) {
-    return this.prisma.user.delete({ where: { id } });
+    try {
+      await this.prisma.user.delete({ where: { id } });
+    } catch (error) {
+      handlePrismaError(error);
+    }
   }
 }
